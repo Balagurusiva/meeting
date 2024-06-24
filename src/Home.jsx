6b03fc6d8f1d6f2fd9f302c59a04744590c92748
balaguru <balagurusiva2002@gmail.com>
2024-06-24 16:50:33 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/Home.jsx 	new file:   src/Meet.jsx 	modified:   src/style.css

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    const [link, setLink] = useState()
+
+  return (
+    <div style={{width:'100% ', height:"100vh", display:'flex', justifyContent:'center', alignItems:"center"}}>
+       <Link to='/meet' style={{backgroundColor:'blue', height:"50px", borderRadius:'20px', padding:'10px', color:'white' }}>New Meeting</Link>
+
+
+       <input type="text" style={{backgroundColor:'white', height:"50px", borderRadius:'20px',   }} placeholder='Enter the link' onChange={e => setLink(e.target.value)} />
+       <Link to={link} style={{backgroundColor:'blue', height:"50px", borderRadius:'20px', padding:'10px', color:'white' }}>Join Meeting</Link>
+    </div>
+  )
+}
+
+export default Home
